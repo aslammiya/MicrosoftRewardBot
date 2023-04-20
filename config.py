8b38ac5071f8e448a4ee4056aad9b5aadc35dbce
Aslam Miya <64162091+aslammiya@users.noreply.github.com>
2023-04-21 00:50:56 +0530

    addedd config file

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 # emails = [""]
 # passwords = [""]
 
-emails = ["aslammiya12372@outlook.com", "aslammiya007@outlook.com", "aslammiya786@outlook.com","aslammiya5257@gmail.com","aslammiya6362@gmail.com"]
+emails = ["aslammiya786@outlook.com","aslammiya12372@outlook.com", "aslammiya007@outlook.com", "aslammiya5257@gmail.com","aslammiya6362@gmail.com"]
 passwords = ["123@Aslam", "123@Aslam", "123@Aslam", "123@Aslam", "123@Aslam"]
 
 
 pc_numOfSearch = 36
 mobile_numOfSearch = 22
 last4Digit = 4116
+
+
+########## REWARD NUMBERS ##########
+# aslammiya786@outlook.com ---> 9820654116 My Number
+# aslammiya12372@outlook.com ---> 9820654116 My Number
+# aslammiya007@outlook.com ---> 9653609384 Ammi's Number
+# aslammiya5257@gmail.com ---> 
+# aslammiya6362@gmail.com ---> 
